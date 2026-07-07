9d9644fb78f0b7b3f236b24b5b0e638df7b78ce8
alfian <alfiansakuragi@gmail.com>
2026-07-07 19:40:16 +0800

    restruktured nested Code: 1.6

--- script.js
+++ script.js
@@ -13,60 +13,111 @@ const deleteAll = $("deleteAll");
 const total = $("total");
 const currentTask = $("currentTask");
 const doneTask = $("doneTask");
+const position = $("position");
 
+// CONTAINER ID
 const container = $("container");
 const completeTask = $("completeTask");
 
 let taskList = JSON.parse(localStorage.getItem("taskList")) || [];
 let taskComplete = JSON.parse(localStorage.getItem("taskComplete")) || [];
 
-doneTask.addEventListener("click", () => {
-    container.style.display = "none";
-    completeTask.style.display = "block";
-})
+const switchToggle = () => {
+    doneTask.addEventListener("click", () => {
+        container.style.display = "none";
+        completeTask.style.display = "block";
+    })
+    
+    currentTask.addEventListener("click", () => {
+        completeTask.style.display = "none";
+        container.style.display = "block";
+    })
+    total.textContent = `${taskList.length}`
+}
+const addTask = () => {
+    form.addEventListener("submit", (e) => {
+        e.preventDefault(); //RESET HTML BEHAVIOR TO REFRESH PAGE
 
-currentTask.addEventListener("click", () => {
-    completeTask.style.display = "none";
-    container.style.display = "block";
-})
-total.textContent = `${taskList.length}`
+        const newTask = {
+            task: task.value,
+            desk: desk.value,
+            level: level.value,
+            author: author.value,
+            due: due.value,
+            position: position.value,
+        };
 
-
-
-// NOTIFICATION BEFORE DELETE ALL DATA
-deleteAll.addEventListener("click", () => {
-    const pilihan = prompt("YAKIN MAU HAPUS SEMUA?? KETIK YA UNTUK MELANJUTKAN");
-    if (pilihan && pilihan.toUpperCase() === "YA") {
-        localStorage.removeItem("taskList");
-        localStorage.removeItem("taskComplete");
-        taskComplete = [];
-        taskList = [];
+        taskList.push(newTask);
+        localStorage.setItem("taskList", JSON.stringify(taskList));
+        total.textContent = `${taskList.length}`
         render();
         renderComplete();
-    } else {
-        console.log("gagal");
-    }
-})
+    });
+}
+const deleteAllTask = () => {
+    deleteAll.addEventListener("click", () => {
+        const pilihan = prompt("YAKIN MAU HAPUS SEMUA?? KETIK YA UNTUK MELANJUTKAN");
+        if (pilihan && pilihan.toUpperCase() === "YA") {
+            localStorage.removeItem("taskList");
+            localStorage.removeItem("taskComplete");
+            taskComplete = [];
+            taskList = [];
+            render();
+            renderComplete();
+        } else {
+            console.log("gagal");
+        }
+    })
+}
+const deleteHistory = () => {
+    const clear = document.querySelectorAll(".clear");
+    clear.forEach((button) => {
+        button.addEventListener("click", (e) => {
+            const indexTarget = e.currentTarget.getAttribute("data-index");
+            taskComplete.splice(indexTarget, 1);
+            localStorage.setItem("taskComplete", JSON.stringify(taskComplete));
+            renderComplete();
+        })
+    })
 
-// EVENT LISTENER FORM;
-form.addEventListener("submit", (e) => {
-    e.preventDefault(); //RESET HTML BEHAVIOR TO REFRESH PAGE
+}
+const deleteTask = () => {
+    const del = document.querySelectorAll(".delete");
+    console.log(del)
+    del.forEach((button) => {
+        button.addEventListener("click", (e) => {
+            const indexTarget = e.currentTarget.getAttribute("data-index");
+            taskList.splice(indexTarget, 1);
+            localStorage.setItem("taskList", JSON.stringify(taskList));
+            total.textContent = `${taskList.length}`
+            render();
+            renderComplete();
+        })
+    })
+}
+const switchTask = () => {
+    const done = document.querySelectorAll(".done");
+    done.forEach((button) => {
+        button.addEventListener("click", (e) => {
+            const card = e.currentTarget.closest(".task-card");
+            const checkbox = card.querySelector('input[type="checkbox"]');
+            const index = Number(e.currentTarget.dataset.index);
+            if (checkbox.checked) {
+                const taskDone = taskList[index];
+                taskList.splice(index, 1);
+                taskComplete.push(taskDone);
+                localStorage.setItem("taskList", JSON.stringify(taskList));
+                localStorage.setItem("taskComplete", JSON.stringify(taskComplete));
+                total.textContent = `${taskList.length}`
+                render();
+                renderComplete();
+            } else {
+                alert("Centang terlebih dahulu");
+            }
+        });
+    })
 
-    const newTask = {
-        task: task.value,
-        desk: desk.value,
-        level: level.value,
-        author: author.value,
-        due: due.value
-    };
-
-    taskList.push(newTask);
-    localStorage.setItem("taskList", JSON.stringify(taskList));
-    total.textContent = `${taskList.length}`
-    render();
-    renderComplete();
-});
-
+}
 const renderComplete = () => {
     completeTask.innerHTML = "";
     taskComplete.forEach((item, index) => {
@@ -80,8 +131,10 @@ const renderComplete = () => {
                         </div>
                     </div>
                     <h2 style="color: red" class="desk" >Telah di selesaikan</h2>
+                    <p>tugas dari: ${item.position}</p>
                     <div class="detail">
                         <p><span class="icon"><ion-icon name="person-outline"></ion-icon></span>Author: ${item.author}</p>
+                        <p><span class="icon"><ion-icon name="man"></ion-icon></span>Position:${item.position}</p>
                         <p><span class="icon"><ion-icon name="calendar-clear-outline"></ion-icon></span>Due: ${item.due}</p>
                     </div>
                     <div class="action">
@@ -91,20 +144,11 @@ const renderComplete = () => {
             </div>
         `
         completeTask.innerHTML += completePage;
+        deleteHistory();
 
-        const clear = document.querySelectorAll(".clear");
-        clear.forEach((button) => {
-            button.addEventListener("click", (e) => {
-                const indexTarget = e.currentTarget.getAttribute("data-index");
-                taskComplete.splice(indexTarget, 1);
-                localStorage.setItem("taskComplete", JSON.stringify(taskComplete));
-                renderComplete();
-            })
-        })
     })
 
 }
-
 const render = () => {
     container.innerHTML = "";
     taskList.forEach((item, index) => {
@@ -123,9 +167,9 @@ const render = () => {
                     </div>
                     <p class="desk" >${item.desk}</p>
                     <div class="detail">
-                        <p><span class="icon"><ion-icon name="person-outline"></ion-icon></span>${item.author}</p>
-                        <p><span class="icon"><ion-icon name="calendar-clear-outline"></ion-icon></span>Due:
-                            ${item.due}</p>
+                        <p><span class="icon"><ion-icon name="person-outline"></ion-icon></span>Author: ${item.author}</p>
+                        <p><span class="icon"><ion-icon name="man"></ion-icon></span>Position:${item.position}</p>
+                        <p><span class="icon"><ion-icon name="calendar-clear-outline"></ion-icon></span>Due:${item.due}</p>
                     </div>
                     <div class="action">
                         <button class="done" data-index="${index}"><span><ion-icon name="checkmark-circle"></ion-icon></span> Task Complete</button>
@@ -135,43 +179,16 @@ const render = () => {
             </div>
         `
         container.innerHTML += card;
-        const del = document.querySelectorAll(".delete");
-        const done = document.querySelectorAll(".done");
-        console.log(del)
-        del.forEach((button) => {
-            button.addEventListener("click", (e) => {
-                const indexTarget = e.currentTarget.getAttribute("data-index");
-                taskList.splice(indexTarget, 1);
-                localStorage.setItem("taskList", JSON.stringify(taskList));
-                render();
-                renderComplete();
-            })
-        })
-
-        done.forEach((button) => {
-            button.addEventListener("click", (e) => {
-                const card = e.currentTarget.closest(".task-card");
-                const checkbox = card.querySelector('input[type="checkbox"]');
-                const index = Number(e.currentTarget.dataset.index);
-                if (checkbox.checked) {
-                    const taskDone = taskList[index];
-                    taskList.splice(index, 1);
-                    taskComplete.push(taskDone);
-                    localStorage.setItem("taskList", JSON.stringify(taskList));
-                    localStorage.setItem("taskComplete", JSON.stringify(taskComplete));
-                    render();
-                    renderComplete();
-                } else {
-                    alert("Centang terlebih dahulu");
-                }
-            });
-        })
-
+        deleteTask();
+        switchTask();
     });
 }
 
-
-
-
+switchToggle();
+addTask();
+deleteAllTask();
 renderComplete();
 render();
+
+
+
